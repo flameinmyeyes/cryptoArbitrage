76f10cd28cafcc9dd5473fdb6cd35a9122360b73
flameinmyeyes <strelokruslan@mail.ru>
2023-06-10 19:32:57 +0300

    Update

---
+++ src/test/java/Kraken.java
@@ -0,0 +1,5 @@
+public class Kraken {
+
+    public static final String BASE_URI = "https://api.kraken.com";
+
+}
--- src/test/java/Kucoin.java
+++ src/test/java/Kucoin.java
@@ -1,23 +1,5 @@
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-
 public class Kucoin {
 
-    public static void main(String[] args) {
-
-
-        Response response = RestAssured
-                .given()
-                    .baseUri("https://api.kucoin.com")
-                    .basePath("/api/v1/prices")
-                .when()
-                    .get()
-                .then()
-//                    .log().all()
-                    .assertThat().statusCode(200)
-                    .extract().response();
-
-        System.out.println(response.getBody().asString());
-    }
+    public static final String BASE_URI = "https://api.kucoin.com";
 
 }
